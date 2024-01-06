9f83c6d9f73627a6bf2db9b7d3117d38ed066bf2
Lucas Cesca <lucascesca14@gmail.com>
2024-01-06 09:48:00 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entitites.Department;
+import model.entitites.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
     }
 }
