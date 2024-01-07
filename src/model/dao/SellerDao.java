ad85d4002cbee31b93f463c28d8bd469a8a017ff
Lucas Cesca <lucascesca14@gmail.com>
2024-01-07 10:57:26 -0300

    DAO interface implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,11 +5,6 @@ import model.entities.Seller;
 
 import java.util.List;
 
-public interface SellerDao {
-    void insert(Seller obj);
-    void update(Seller obj);
-    void deleteById(Integer id);
-    Seller findById(Integer id);
-    List<Seller> findAll();
+public interface SellerDao extends DAO<Seller>{
     List<Seller> findByDepartment(Department department);
 }
